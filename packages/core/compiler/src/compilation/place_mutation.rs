eda24e340b658a86d6f2de1d37c3a2d6b53f6538
takeokunn <bararararatty@gmail.com>
2026-09-01 03:44:27 +0900

    refactor compiler place mutation helpers

--- packages/core/compiler/src/compilation/place_mutation.rs
+++ packages/core/compiler/src/compilation/place_mutation.rs
@@ -1,6 +1,8 @@
 #![allow(clippy::wildcard_imports)]
 use super::*;
 
+mod modify;
+
 impl CompileState {
     pub(crate) fn compile_setf(
         &mut self,
@@ -353,45 +355,6 @@ impl CompileState {
                 self.emit(function, Instruction::Pop, value_form.span)?;
             }
         }
-        Ok(())
-    }
-
-    pub(crate) fn compile_modify_symbol(
-        &mut self,
-        function: FunctionId,
-        span: Span,
-        items: &[Form],
-        operator: &str,
-        arithmetic: &str,
-    ) -> Result<(), CompileError> {
-        if !(items.len() == 2 || items.len() == 3) {
-            return Err(Self::arity_error(items, operator, "one or two", span));
-        }
-        let place = items
-            .get(1)
-            .ok_or_else(|| Self::internal_error(span, "missing modifying place"))?;
-        let (name, escaped) = Self::symbol_name_info(place, &format!("{operator} target"))?;
-        self.emit(
-            function,
-            Instruction::FunctionLoad(arithmetic.to_string()),
-            place.span,
-        )?;
-        self.compile_expression(function, place)?;
-        if let Some(delta) = items.get(2) {
-            self.compile_expression(function, delta)?;
-        } else {
-            self.emit(function, Instruction::Constant(Constant::Integer(1)), span)?;
-        }
-        self.emit(function, Instruction::Call(2), span)?;
-        self.emit(
-            function,
-            if escaped {
-                Instruction::SetExact(name)
-            } else {
-                Instruction::Set(name)
-            },
-            place.span,
-        )?;
         Ok(())
     }
 }

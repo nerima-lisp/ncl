3557104c161008d06e0f8ce1c3bd3bc01854b5af
takeokunn <bararararatty@gmail.com>
2026-09-01 23:55:35 +0900

    fix: route character predicates to native instructions

--- packages/core/compiler/src/forms/special_forms_bindings.rs
+++ packages/core/compiler/src/forms/special_forms_bindings.rs
@@ -329,7 +329,7 @@ impl CompileState {
             | "SIMPLE-VECTOR-P" | "BIT-VECTOR-P" | "SIMPLE-BIT-VECTOR-P" | "ARRAYP"
             | "SIMPLE-ARRAY-P" | "HASH-TABLE-P" | "RANDOM-STATE-P" | "STREAMP" | "INPUT-STREAM-P"
             | "NOT" | "NULL"
-            | "OUTPUT-STREAM-P" => {
+            | "OUTPUT-STREAM-P" | "DIGIT-CHAR-P" => {
                 if name == "DIGIT-CHAR-P" {
                     self.compile_character_digit_predicate(function, span, items)
                 } else {

550fccf6675f2ceb28de1cdc2ad23b3f4a5c0fc6
takeokunn <bararararatty@gmail.com>
2026-09-01 21:58:38 +0900

    refactor: split compiler call invocation methods

--- packages/core/compiler/src/compilation/invocation.rs
+++ packages/core/compiler/src/compilation/invocation.rs
@@ -1,65 +1,10 @@
 #![allow(clippy::wildcard_imports)]
 use super::*;
 
+#[path = "invocation_calls.rs"]
+mod invocation_calls;
+
 impl CompileState {
-    pub(crate) fn compile_funcall(
-        &mut self,
-        function: FunctionId,
-        span: Span,
-        items: &[Form],
-    ) -> Result<(), CompileError> {
-        if items.len() < 2 {
-            return Err(Self::arity_error(items, "FUNCALL", "at least one", span));
-        }
-        for item in &items[1..] {
-            self.compile_expression(function, item)?;
-        }
-        self.emit(
-            function,
-            Instruction::Call(items.len().saturating_sub(2)),
-            span,
-        )?;
-        Ok(())
-    }
-
-    pub(crate) fn compile_eval(
-        &mut self,
-        function: FunctionId,
-        span: Span,
-        items: &[Form],
-    ) -> Result<(), CompileError> {
-        Self::require_arity(items, "EVAL", "one", 1, span)?;
-        let Some(argument) = items.get(1) else {
-            return Err(Self::internal_error(
-                span,
-                "missing eval argument after arity check",
-            ));
-        };
-        self.compile_expression(function, argument)?;
-        self.emit(function, Instruction::Eval(argument.span), span)?;
-        Ok(())
-    }
-
-    pub(crate) fn compile_apply(
-        &mut self,
-        function: FunctionId,
-        span: Span,
-        items: &[Form],
-    ) -> Result<(), CompileError> {
-        if items.len() < 3 {
-            return Err(Self::arity_error(items, "APPLY", "at least two", span));
-        }
-        for item in &items[1..] {
-            self.compile_expression(function, item)?;
-        }
-        self.emit(
-            function,
-            Instruction::Apply(items.len().saturating_sub(2)),
-            span,
-        )?;
-        Ok(())
-    }
-
     pub(crate) fn compile_list_mapping(
         &mut self,
         function: FunctionId,

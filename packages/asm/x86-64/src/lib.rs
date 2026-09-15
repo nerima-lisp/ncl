9b73c9e251b29c5803d7e24268a9fda1bd1cdff1
takeokunn <bararararatty@gmail.com>
2026-09-15 09:58:16 +0900

    style(asm): format public exports
    次にやること: SafepointMap 生成と compile_function の契約を完成させる

--- packages/asm/x86-64/src/lib.rs
+++ packages/asm/x86-64/src/lib.rs
@@ -8,5 +8,5 @@ mod model;
 mod tests;
 
 pub use assembler::{Assembler, CodeBlob, EncodeError, Fixup, FixupKind};
-pub use disassemble::{decode, display, DecodeError};
+pub use disassemble::{DecodeError, decode, display};
 pub use model::{BinOp, Cond, Imm, Inst, Label, Mem, Reg, Scale, Xmm};

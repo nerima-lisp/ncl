ffe51cadc265c09f8192132a7fadd48e75c21bae
takeokunn <bararararatty@gmail.com>
2026-09-26 04:11:03 +0900

    feat(sequences): implement list and cons operations

--- packages/lib/sequences/src/builtins.rs
+++ packages/lib/sequences/src/builtins.rs
@@ -1,7 +1,7 @@
 use ncl_object::{
     Arity, Builtin, BuiltinArgs, BuiltinConvention, BuiltinIdentifier, BuiltinImplementation,
-    BuiltinName, BuiltinPackage, LambdaList, MultipleValues, ObjectError, Parameter, ParameterType,
-    Runtime, Sequence, ThreadContext, Word, typed_builtin,
+    BuiltinName, BuiltinPackage, FromLispArg, LambdaList, MultipleValues, ObjectError, Parameter,
+    ParameterType, Runtime, Sequence, ThreadContext, Word, typed_builtin,
 };
 
 use crate::domain;
@@ -131,6 +131,20 @@ const TWO_CONS_VALUE: &[Parameter] = &[
     parameter("cons", ParameterType::Any),
     parameter("value", ParameterType::Any),
 ];
+const THREE_LISTS: &[Parameter] = &[
+    parameter("keys", ParameterType::List),
+    parameter("data", ParameterType::List),
+    parameter("alist", ParameterType::List),
+];
+const THREE_ANY: &[Parameter] = &[
+    parameter("first", ParameterType::Any),
+    parameter("second", ParameterType::Any),
+    parameter("third", ParameterType::Any),
+];
+const TWO_FUNCTION_LIST: &[Parameter] = &[
+    parameter("predicate", ParameterType::FunctionDesignator),
+    parameter("alist", ParameterType::List),
+];
 const SEQUENCE_INDEX: &[Parameter] = &[
     parameter("sequence", ParameterType::Sequence),
     parameter("index", ParameterType::Fixnum),
@@ -167,6 +181,184 @@ typed_builtin!(member_builtin, domain::member, (item: Word, value: ncl_object::L
 typed_builtin!(assoc_builtin, domain::assoc, (item: Word, value: ncl_object::List));
 typed_builtin!(rplaca_builtin, domain::rplaca, (cons: Word, value: Word));
 typed_builtin!(rplacd_builtin, domain::rplacd, (cons: Word, value: Word));
+typed_builtin!(cons_builtin, domain::cons, (car: Word, cdr: Word));
+typed_builtin!(copy_list_builtin, domain::copy_list, (value: ncl_object::List));
+typed_builtin!(copy_tree_builtin, domain::copy_tree, (value: Word));
+typed_builtin!(rassoc_builtin, domain::rassoc, (item: Word, alist: ncl_object::List));
+typed_builtin!(caar_builtin, domain::caar, (value: Word));
+typed_builtin!(cadr_builtin, domain::cadr, (value: Word));
+typed_builtin!(cdar_builtin, domain::cdar, (value: Word));
+typed_builtin!(cddr_builtin, domain::cddr, (value: Word));
+typed_builtin!(caaar_builtin, domain::caaar, (value: Word));
+typed_builtin!(caadr_builtin, domain::caadr, (value: Word));
+typed_builtin!(cadar_builtin, domain::cadar, (value: Word));
+typed_builtin!(caddr_builtin, domain::caddr, (value: Word));
+typed_builtin!(cdaar_builtin, domain::cdaar, (value: Word));
+typed_builtin!(cdadr_builtin, domain::cdadr, (value: Word));
+typed_builtin!(cddar_builtin, domain::cddar, (value: Word));
+typed_builtin!(cdddr_builtin, domain::cdddr, (value: Word));
+typed_builtin!(caaaar_builtin, domain::caaaar, (value: Word));
+typed_builtin!(caaadr_builtin, domain::caaadr, (value: Word));
+typed_builtin!(caadar_builtin, domain::caadar, (value: Word));
+typed_builtin!(caaddr_builtin, domain::caaddr, (value: Word));
+typed_builtin!(cadaar_builtin, domain::cadaar, (value: Word));
+typed_builtin!(cadadr_builtin, domain::cadadr, (value: Word));
+typed_builtin!(caddar_builtin, domain::caddar, (value: Word));
+typed_builtin!(cadddr_builtin, domain::cadddr, (value: Word));
+typed_builtin!(cdaaar_builtin, domain::cdaaar, (value: Word));
+typed_builtin!(cdaadr_builtin, domain::cdaadr, (value: Word));
+typed_builtin!(cdadar_builtin, domain::cdadar, (value: Word));
+typed_builtin!(cdaddr_builtin, domain::cdaddr, (value: Word));
+typed_builtin!(cddaar_builtin, domain::cddaar, (value: Word));
+typed_builtin!(cddadr_builtin, domain::cddadr, (value: Word));
+typed_builtin!(cdddar_builtin, domain::cdddar, (value: Word));
+typed_builtin!(cddddr_builtin, domain::cddddr, (value: Word));
+
+fn adapted<F>(args: &BuiltinArgs<'_>, f: F) -> Result<Word, ncl_object::LispError>
+where
+    F: FnOnce(&[Word]) -> Result<Word, ncl_object::LispError>,
+{
+    f((0..args.len())
+        .map(|i| args.get(i).expect("builtin argument index"))
+        .collect::<Vec<_>>()
+        .as_slice())
+    .map_err(|error| error)
+}
+fn list_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let result = adapted(args, |v| domain::list(ctx, runtime, v));
+    map_lisp(ctx, result)
+}
+fn list_star_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let result = adapted(args, |v| domain::list_star(ctx, runtime, v));
+    map_lisp(ctx, result)
+}
+fn append_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let result = adapted(args, |v| domain::append(ctx, runtime, v));
+    map_lisp(ctx, result)
+}
+fn map_lisp(
+    ctx: &mut ThreadContext,
+    result: Result<Word, ncl_object::LispError>,
+) -> Result<Word, ObjectError> {
+    result.map_err(|error| {
+        ctx.set_pending_lisp_error(error);
+        ObjectError::TypeError
+    })
+}
+
+fn pass_arguments(args: &BuiltinArgs<'_>) -> Result<Vec<Word>, ObjectError> {
+    (0..args.len())
+        .map(|index| args.get(index).ok_or(ObjectError::TypeError))
+        .collect()
+}
+fn acons_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let result = domain::acons(
+        ctx,
+        runtime,
+        args.required(0)?,
+        args.required(1)?,
+        ncl_object::List::from_lisp_arg(ctx, args.required(2)?)
+            .map_err(|_| ObjectError::TypeError)?,
+    );
+    map_lisp(ctx, result)
+}
+fn pairlis_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let result = domain::pairlis(
+        ctx,
+        runtime,
+        ncl_object::List::from_lisp_arg(ctx, args.required(0)?)
+            .map_err(|_| ObjectError::TypeError)?,
+        ncl_object::List::from_lisp_arg(ctx, args.required(1)?)
+            .map_err(|_| ObjectError::TypeError)?,
+        ncl_object::List::from_lisp_arg(ctx, args.required(2)?)
+            .map_err(|_| ObjectError::TypeError)?,
+    );
+    map_lisp(ctx, result)
+}
+fn getf_builtin(
+    ctx: &mut ThreadContext,
+    _: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let result = domain::getf(
+        ctx,
+        ncl_object::List::from_lisp_arg(ctx, args.required(0)?)
+            .map_err(|_| ObjectError::TypeError)?,
+        args.required(1)?,
+        args.get(2).unwrap_or(Word::NIL),
+    );
+    map_lisp(ctx, result)
+}
+fn assoc_if_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let list = ncl_object::List::from_lisp_arg(ctx, args.required(1)?)
+        .map_err(|_| ObjectError::TypeError)?;
+    let result = domain::assoc_if(ctx, runtime, args.required(0)?, list, false);
+    map_lisp(ctx, result)
+}
+fn assoc_if_not_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let list = ncl_object::List::from_lisp_arg(ctx, args.required(1)?)
+        .map_err(|_| ObjectError::TypeError)?;
+    let result = domain::assoc_if(ctx, runtime, args.required(0)?, list, true);
+    map_lisp(ctx, result)
+}
+fn rassoc_if_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let list = ncl_object::List::from_lisp_arg(ctx, args.required(1)?)
+        .map_err(|_| ObjectError::TypeError)?;
+    let result = domain::rassoc_if(ctx, runtime, args.required(0)?, list, false);
+    map_lisp(ctx, result)
+}
+fn rassoc_if_not_builtin(
+    ctx: &mut ThreadContext,
+    runtime: &Runtime,
+    args: &BuiltinArgs<'_>,
+    _: &mut MultipleValues,
+) -> Result<Word, ObjectError> {
+    let list = ncl_object::List::from_lisp_arg(ctx, args.required(1)?)
+        .map_err(|_| ObjectError::TypeError)?;
+    let result = domain::rassoc_if(ctx, runtime, args.required(0)?, list, true);
+    map_lisp(ctx, result)
+}
 
 pub(crate) fn entry(name: &str) -> Option<BuiltinImplementation> {
     let implementation: (Builtin, ncl_object::RustBuiltin) = match name {
@@ -183,6 +375,66 @@ pub(crate) fn entry(name: &str) -> Option<BuiltinImplementation> {
         "ASSOC" => (descriptor(TWO_ITEM_ALIST), assoc_builtin),
         "RPLACA" => (descriptor(TWO_CONS_VALUE), rplaca_builtin),
         "RPLACD" => (descriptor(TWO_CONS_VALUE), rplacd_builtin),
+        "CONS" => (descriptor(TWO_CONS_VALUE), cons_builtin),
+        "COPY-LIST" => (descriptor(ONE_LIST), copy_list_builtin),
+        "COPY-TREE" => (descriptor(ONE_ANY), copy_tree_builtin),
+        "RASSOC" => (descriptor(TWO_ITEM_ALIST), rassoc_builtin),
+        "CAAR" => (descriptor(ONE_ANY), caar_builtin),
+        "CADR" => (descriptor(ONE_ANY), cadr_builtin),
+        "CDAR" => (descriptor(ONE_ANY), cdar_builtin),
+        "CDDR" => (descriptor(ONE_ANY), cddr_builtin),
+        "CAAAR" => (descriptor(ONE_ANY), caaar_builtin),
+        "CAADR" => (descriptor(ONE_ANY), caadr_builtin),
+        "CADAR" => (descriptor(ONE_ANY), cadar_builtin),
+        "CADDR" => (descriptor(ONE_ANY), caddr_builtin),
+        "CDAAR" => (descriptor(ONE_ANY), cdaar_builtin),
+        "CDADR" => (descriptor(ONE_ANY), cdadr_builtin),
+        "CDDAR" => (descriptor(ONE_ANY), cddar_builtin),
+        "CDDDR" => (descriptor(ONE_ANY), cdddr_builtin),
+        "CAAAAR" => (descriptor(ONE_ANY), caaaar_builtin),
+        "CAAADR" => (descriptor(ONE_ANY), caaadr_builtin),
+        "CAADAR" => (descriptor(ONE_ANY), caadar_builtin),
+        "CAADDR" => (descriptor(ONE_ANY), caaddr_builtin),
+        "CADAAR" => (descriptor(ONE_ANY), cadaar_builtin),
+        "CADADR" => (descriptor(ONE_ANY), cadadr_builtin),
+        "CADDAR" => (descriptor(ONE_ANY), caddar_builtin),
+        "CADDDR" => (descriptor(ONE_ANY), cadddr_builtin),
+        "CDAAAR" => (descriptor(ONE_ANY), cdaaar_builtin),
+        "CDAADR" => (descriptor(ONE_ANY), cdaadr_builtin),
+        "CDADAR" => (descriptor(ONE_ANY), cdadar_builtin),
+        "CDADDR" => (descriptor(ONE_ANY), cdaddr_builtin),
+        "CDDAAR" => (descriptor(ONE_ANY), cddaar_builtin),
+        "CDDADR" => (descriptor(ONE_ANY), cddadr_builtin),
+        "CDDDAR" => (descriptor(ONE_ANY), cdddar_builtin),
+        "CDDDDR" => (descriptor(ONE_ANY), cddddr_builtin),
+        "LIST" => (
+            Builtin {
+                lambda_list: LambdaList::with_rest(&[], parameter("arguments", ParameterType::Any)),
+                convention: BuiltinConvention::Adapted,
+            },
+            list_builtin,
+        ),
+        "LIST*" => (
+            Builtin {
+                lambda_list: LambdaList::with_rest(&[], parameter("arguments", ParameterType::Any)),
+                convention: BuiltinConvention::Adapted,
+            },
+            list_star_builtin,
+        ),
+        "APPEND" => (
+            Builtin {
+                lambda_list: LambdaList::with_rest(&[], parameter("arguments", ParameterType::Any)),
+                convention: BuiltinConvention::Adapted,
+            },
+            append_builtin,
+        ),
+        "ACONS" => (descriptor(THREE_ANY), acons_builtin),
+        "PAIRLIS" => (descriptor(THREE_LISTS), pairlis_builtin),
+        "GETF" => (descriptor(THREE_ANY), getf_builtin),
+        "ASSOC-IF" => (descriptor(TWO_FUNCTION_LIST), assoc_if_builtin),
+        "ASSOC-IF-NOT" => (descriptor(TWO_FUNCTION_LIST), assoc_if_not_builtin),
+        "RASSOC-IF" => (descriptor(TWO_FUNCTION_LIST), rassoc_if_builtin),
+        "RASSOC-IF-NOT" => (descriptor(TWO_FUNCTION_LIST), rassoc_if_not_builtin),
         "LENGTH" => (descriptor(ONE_ANY), generic_length),
         "ELT" => (
             Builtin {
@@ -224,10 +476,14 @@ pub(crate) fn entry(name: &str) -> Option<BuiltinImplementation> {
         ),
         _ => return None,
     };
-    Some(BuiltinImplementation::direct(
-        implementation.0,
-        implementation.1,
-    ))
+    Some(match implementation.0.convention {
+        BuiltinConvention::Direct(_) => {
+            BuiltinImplementation::direct(implementation.0, implementation.1)
+        }
+        BuiltinConvention::Adapted => {
+            BuiltinImplementation::adapted(implementation.0, implementation.1, pass_arguments)
+        }
+    })
 }
 
 pub(crate) fn unsupported(

d31b7003c4358989e3d57c2d65034ac7b75f6bca
takeokunn <bararararatty@gmail.com>
2026-09-26 03:15:58 +0900

    test(object): expand object layer coverage

--- packages/object/src/classify.rs
+++ packages/object/src/classify.rs
@@ -1,5 +1,5 @@
-use crate::ThreadContext;
 use crate::layout::widetag;
+use crate::ThreadContext;
 use ncl_sys::{LowTag, Word};
 
 /// Classification of a tagged value.
@@ -34,11 +34,11 @@ pub enum ObjectRef {
 /// Classify a tagged value using lowtag information.
 #[must_use]
 pub fn classify(word: Word) -> ObjectRef {
-    if let Some(value) = word.as_fixnum() {
-        return ObjectRef::Fixnum(value);
-    }
     if word.is_character() {
         return ObjectRef::Character(u32::try_from(word.bits() >> 4).unwrap_or(0));
+    }
+    if let Some(value) = word.as_fixnum() {
+        return ObjectRef::Fixnum(value);
     }
     match word.lowtag() {
         x if x == LowTag::List as u8 => {

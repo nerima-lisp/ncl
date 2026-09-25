a7a4ae157923444566e7e063124e5217d82aee95
takeokunn <bararararatty@gmail.com>
2026-09-26 02:13:52 +0900

    test(asm-aarch64): split coverage encoder tests

---
+++ packages/asm/aarch64/tests/decoder.rs
@@ -0,0 +1,179 @@
+#![allow(missing_docs)]
+#![allow(clippy::unwrap_used)]
+
+use super::common::*;
+use ncl_asm_aarch64::*;
+
+#[test]
+fn decoder_covers_supported_instruction_families() {
+    let r0 = x(0);
+    let r1 = x(1);
+    let label = Label(0);
+    let words = [
+        encode(
+            &Inst::MovZ {
+                rd: r0,
+                imm: 7,
+                shift: 16,
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::AddImm {
+                rd: RegOrSp::Sp,
+                rn: RegOrSp::Sp,
+                imm: 7,
+                shift: true,
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::SubImm {
+                rd: RegOrSp::Reg(r0),
+                rn: RegOrSp::Reg(r1),
+                imm: 7,
+                shift: false,
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Ldr {
+                rt: r0,
+                mem: ncl_asm_aarch64::MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                    scale: 8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Str {
+                rt: r0,
+                mem: ncl_asm_aarch64::MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                    scale: 8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Ldr {
+                rt: r0,
+                mem: ncl_asm_aarch64::MemOperand::Unscaled {
+                    base: RegOrSp::Sp,
+                    offset: -8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Str {
+                rt: r0,
+                mem: ncl_asm_aarch64::MemOperand::PreIndex {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Ldr {
+                rt: r0,
+                mem: ncl_asm_aarch64::MemOperand::PostIndex {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Cmp {
+                rn: r0,
+                rm: r1,
+                shift: Shift::Lsl(0),
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Mov {
+                rd: RegOrSp::Reg(r0),
+                rn: RegOrSp::Reg(r1),
+            },
+            0,
+        )
+        .unwrap(),
+        encode(&Inst::Blr { rn: r1 }, 0).unwrap(),
+        encode(&Inst::Adr { rd: r0, label }, 0).unwrap(),
+        encode(&Inst::Adrp { rd: r0, label }, 0).unwrap(),
+        encode(&Inst::Cbz { rt: r0, label }, 0).unwrap(),
+        encode(&Inst::Cbnz { rt: r0, label }, 0).unwrap(),
+        encode(&Inst::Brk { imm: 7 }, 0).unwrap(),
+        encode(&Inst::B { label }, 0).unwrap(),
+        encode(
+            &Inst::BCond {
+                cond: Cond::Al,
+                label,
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Ldp {
+                rt: r0,
+                rt2: r1,
+                mem: ncl_asm_aarch64::MemOperand::Unscaled {
+                    base: RegOrSp::Sp,
+                    offset: 0,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Ldp {
+                rt: r0,
+                rt2: r1,
+                mem: ncl_asm_aarch64::MemOperand::PreIndex {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+        encode(
+            &Inst::Stp {
+                rt: r0,
+                rt2: r1,
+                mem: ncl_asm_aarch64::MemOperand::PostIndex {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                },
+            },
+            0,
+        )
+        .unwrap(),
+    ];
+    for word in words {
+        assert!(decode(word).is_ok(), "decoder rejected 0x{word:08x}");
+    }
+    for value in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] {
+        let word = 0x5400_0000 | value;
+        assert!(decode(word).is_ok());
+    }
+    assert!(matches!(
+        decode(0),
+        Err(ncl_asm_aarch64::EncodeError::UnsupportedInstruction(0))
+    ));
+}
---
+++ packages/asm/aarch64/tests/encoder_edges.rs
@@ -0,0 +1,351 @@
+#![allow(missing_docs)]
+#![allow(clippy::unwrap_used)]
+
+use super::common::*;
+use ncl_asm_aarch64::*;
+
+#[test]
+fn addressing_logical_shift_and_float_edges_are_observed() {
+    use ncl_asm_aarch64::{EncodeError, Extend, MemOperand, VReg};
+
+    let r0 = x(0);
+    let r1 = x(1);
+    let scaled = [
+        (
+            Inst::Ldr {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                    scale: 8,
+                },
+            },
+            0xF940_07E0,
+        ),
+        (
+            Inst::Str {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 8,
+                    scale: 8,
+                },
+            },
+            0xF900_07E0,
+        ),
+        (
+            Inst::LdrW {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 4,
+                    scale: 4,
+                },
+            },
+            0xB940_07E0,
+        ),
+        (
+            Inst::StrW {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 4,
+                    scale: 4,
+                },
+            },
+            0xB900_07E0,
+        ),
+        (
+            Inst::Ldrb {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 1,
+                    scale: 1,
+                },
+            },
+            0x3940_07E0,
+        ),
+        (
+            Inst::Strb {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 1,
+                    scale: 1,
+                },
+            },
+            0x3900_07E0,
+        ),
+        (
+            Inst::Ldrh {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 2,
+                    scale: 2,
+                },
+            },
+            0x7940_07E0,
+        ),
+        (
+            Inst::Strh {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 2,
+                    scale: 2,
+                },
+            },
+            0x7900_07E0,
+        ),
+        (
+            Inst::Ldrsw {
+                rt: r0,
+                mem: MemOperand::Unsigned {
+                    base: RegOrSp::Sp,
+                    offset: 4,
+                    scale: 4,
+                },
+            },
+            0xB940_07E0,
+        ),
+    ];
+    for (index, (instruction, expected)) in scaled.into_iter().enumerate() {
+        assert_eq!(
+            encode(&instruction, 0),
+            Ok(expected),
+            "index {index}: {instruction:?}"
+        );
+    }
+
+    for mem in [
+        MemOperand::Unscaled {
+            base: RegOrSp::Sp,
+            offset: -8,
+        },
+        MemOperand::PreIndex {
+            base: RegOrSp::Sp,
+            offset: 8,
+        },
+        MemOperand::PostIndex {
+            base: RegOrSp::Sp,
+            offset: 8,
+        },
+        MemOperand::Register {
+            base: RegOrSp::Sp,
+            index: r1,
+            extend: None,
+            shift: 0,
+        },
+        MemOperand::Register {
+            base: RegOrSp::Sp,
+            index: r1,
+            extend: Some(Extend::Uxtw),
+            shift: 8,
+        },
+        MemOperand::Register {
+            base: RegOrSp::Sp,
+            index: r1,
+            extend: Some(Extend::Sxtw),
+            shift: 0,
+        },
+        MemOperand::Register {
+            base: RegOrSp::Sp,
+            index: r1,
+            extend: Some(Extend::Sxtx),
+            shift: 8,
+        },
+    ] {
+        assert!(encode(&Inst::Ldr { rt: r0, mem }, 0).is_ok());
+        assert!(encode(&Inst::Str { rt: r0, mem }, 0).is_ok());
+    }
+    for mem in [
+        MemOperand::Unsigned {
+            base: RegOrSp::Sp,
+            offset: 8,
+            scale: 4,
+        },
+        MemOperand::Register {
+            base: RegOrSp::Sp,
+            index: r1,
+            extend: Some(Extend::Sxtb),
+            shift: 1,
+        },
+    ] {
+        assert!(matches!(
+            encode(&Inst::Ldr { rt: r0, mem }, 0),
+            Err(EncodeError::ImmediateOutOfRange { .. })
+        ));
+    }
+
+    for (index, instruction) in [
+        Inst::AndImm {
+            rd: r0,
+            rn: r1,
+            imm: 1,
+        },
+        Inst::OrrImm {
+            rd: r0,
+            rn: r1,
+            imm: 0x00FF_00FF_00FF_00FF,
+        },
+        Inst::EorImm {
+            rd: r0,
+            rn: r1,
+            imm: 0x0000_0000_0000_00FF,
+        },
+        Inst::TstImm { rn: r1, imm: 0xF0 },
+    ]
+    .into_iter()
+    .enumerate()
+    {
+        assert!(
+            encode(&instruction, 0).is_ok(),
+            "logical index {index}: {instruction:?}"
+        );
+    }
+    for amount in [0, 1, 31, 63] {
+        for instruction in [
+            Inst::LslImm {
+                rd: r0,
+                rn: r1,
+                amount,
+            },
+            Inst::LsrImm {
+                rd: r0,
+                rn: r1,
+                amount,
+            },
+            Inst::AsrImm {
+                rd: r0,
+                rn: r1,
+                amount,
+            },
+        ] {
+            assert!(encode(&instruction, 0).is_ok());
+        }
+    }
+    assert!(matches!(
+        encode(
+            &Inst::LslImm {
+                rd: r0,
+                rn: r1,
+                amount: 64
+            },
+            0
+        ),
+        Err(EncodeError::ImmediateOutOfRange { .. })
+    ));
+    assert!(matches!(
+        encode(
+            &Inst::Tbz {
+                rt: r0,
+                bit: 64,
+                label: Label(0)
+            },
+            0
+        ),
+        Err(EncodeError::ImmediateOutOfRange { .. })
+    ));
+
+    let v0 = VReg {
+        number: 0,
+        double: true,
+    };
+    let v1 = VReg {
+        number: 1,
+        double: true,
+    };
+    for instruction in [
+        Inst::Fmov { rd: v0, rn: v1 },
+        Inst::FmovGeneral {
+            v: v0,
+            r: r0,
+            to_float: false,
+        },
+        Inst::Fadd {
+            rd: v0,
+            rn: v1,
+            rm: v0,
+        },
+        Inst::Fsub {
+            rd: v0,
+            rn: v1,
+            rm: v0,
+        },
+        Inst::Fmul {
+            rd: v0,
+            rn: v1,
+            rm: v0,
+        },
+        Inst::Fdiv {
+            rd: v0,
+            rn: v1,
+            rm: v0,
+        },
+        Inst::Fsqrt { rd: v0, rn: v1 },
+        Inst::Fneg { rd: v0, rn: v1 },
+        Inst::Fcmp { rn: v0, rm: v1 },
+        Inst::Scvtf { rd: v0, rn: r0 },
+        Inst::Fcvtzs { rd: r0, rn: v0 },
+        Inst::LdrD {
+            rt: v0,
+            mem: MemOperand::Unsigned {
+                base: RegOrSp::Sp,
+                offset: 8,
+                scale: 8,
+            },
+        },
+        Inst::StrD {
+            rt: v0,
+            mem: MemOperand::Unsigned {
+                base: RegOrSp::Sp,
+                offset: 8,
+                scale: 8,
+            },
+        },
+    ] {
+        assert!(encode(&instruction, 0).is_ok());
+    }
+    assert!(
+        encode(
+            &Inst::Fmov {
+                rd: VReg {
+                    number: 32,
+                    double: true
+                },
+                rn: v1
+            },
+            0
+        )
+        .is_err()
+    );
+    assert!(
+        encode(
+            &Inst::Fadd {
+                rd: v0,
+                rn: VReg {
+                    number: 1,
+                    double: false
+                },
+                rm: v0
+            },
+            0
+        )
+        .is_err()
+    );
+    assert!(
+        encode(
+            &Inst::LdrD {
+                rt: v0,
+                mem: MemOperand::Unscaled {
+                    base: RegOrSp::Sp,
+                    offset: 0
+                }
+            },
+            0
+        )
+        .is_err()
+    );
+}

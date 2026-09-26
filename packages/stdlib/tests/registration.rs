fa5e03d4161b97496d076e0023f6ed9497b50919
takeokunn <bararararatty@gmail.com>
2026-09-26 13:11:56 +0900

    feat(stdlib): register hash arrays

--- packages/stdlib/tests/registration.rs
+++ packages/stdlib/tests/registration.rs
@@ -16,3 +16,19 @@ fn register_all_registers_clos() {
 
     assert!(runtime.class(&mut ctx, "CLASS").is_some());
 }
+
+#[test]
+fn register_all_registers_hash_arrays() {
+    let runtime = Runtime::new().unwrap();
+    let mut ctx = ThreadContext::new();
+    ctx.register(&runtime).unwrap();
+
+    ncl_stdlib::register_all(&mut ctx, &runtime).unwrap();
+
+    assert!(runtime.function(&mut ctx, "COMMON-LISP", "MAKE-ARRAY").is_some());
+    assert!(
+        runtime
+            .function(&mut ctx, "COMMON-LISP", "MAKE-HASH-TABLE")
+            .is_some()
+    );
+}

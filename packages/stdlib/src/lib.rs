abb087de1e6f7542da29cd77dbabbd91d0495ada
takeokunn <bararararatty@gmail.com>
2026-09-26 13:11:56 +0900

    feat(stdlib): register hash arrays

--- packages/stdlib/src/lib.rs
+++ packages/stdlib/src/lib.rs
@@ -45,6 +45,7 @@ pub fn register_all(ctx: &mut ThreadContext, runtime: &Runtime) -> Result<(), Ob
     ncl_printer::register(ctx, runtime)?;
     ncl_conditions::register(runtime)?;
     ncl_clos::register(runtime)?;
+    ncl_lib_hash_arrays::register(runtime)?;
     ncl_lib_streams::register(runtime)?;
     ncl_lib_packages::register(runtime)?;
     ncl_threads::register(runtime)?;

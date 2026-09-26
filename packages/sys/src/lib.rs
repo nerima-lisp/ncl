082c680874e59e1ca9b9a58b931114034a03c1d2
takeokunn <bararararatty@gmail.com>
2026-09-26 10:31:04 +0900

    feat(sys): publish word encoding constants

--- packages/sys/src/lib.rs
+++ packages/sys/src/lib.rs
@@ -26,7 +26,10 @@ pub use heap::{
 pub use invoke::{invoke_entry, invoke_entry_with_function};
 pub use sync::{Condvar, Mutex, Semaphore, WaitQueue};
 pub use thread::{NativeState, RootToken, SafepointState, Thread, ThreadLayout, thread_layout};
-pub use word::{LowTag, Word};
+pub use word::{
+    CHARACTER_SHIFT, CHARACTER_TAG, FIXNUM_TAG, FIXNUM_TAG_BITS, LOWTAG_BITS, LOWTAG_MASK, LowTag,
+    Word,
+};
 
 /// A borrowed precise-root slot whose value the collector may rewrite in place.
 ///

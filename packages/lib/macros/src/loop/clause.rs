d7ac9f3b9455bda9d9f532e0412eecc4fb66723d
takeokunn <bararararatty@gmail.com>
2026-09-27 03:28:29 +0900

    feat(macros): extend loop iteration and accumulation

---
+++ packages/lib/macros/src/loop/clause.rs
@@ -0,0 +1,43 @@
+use super::{AccumulatorKind, LimitDirection, StepDirection};
+
+#[derive(Clone, Debug)]
+pub(super) enum HeldLoopClause {
+    With {
+        variable: usize,
+        init: usize,
+    },
+    For {
+        variable: usize,
+        init: usize,
+        step: Option<usize>,
+        direction: Option<StepDirection>,
+        limit: Option<(LimitDirection, usize)>,
+    },
+    EqualsThen {
+        variable: usize,
+        init: usize,
+        then: usize,
+    },
+    In {
+        variable: usize,
+        sequence: usize,
+        on: bool,
+        by: Option<usize>,
+    },
+    Across {
+        variable: usize,
+        vector: usize,
+    },
+    Repeat(usize),
+    While(usize),
+    Until(usize),
+    Initially(Vec<usize>),
+    Finally(Vec<usize>),
+    Do(Vec<usize>),
+    Accumulate {
+        kind: AccumulatorKind,
+        form: usize,
+        variable: Option<usize>,
+    },
+    Return(usize),
+}
